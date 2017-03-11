10987b76380e2fb65b1e8a8556cf8c580a5c7be0
Eugene Prilepin <esp.home@gmail.com>
2017-03-11 11:54:05 +0300

    Add build setup script

--- setup.py
+++ setup.py
@@ -1,1 +1,30 @@
 # -*- coding: utf-8 -*-
+
+import sys
+from cx_Freeze import setup, Executable
+
+from voicerecorder import __version__
+
+base = None
+if sys.platform == 'win32':
+    base = 'Win32GUI'
+
+options = {
+    'build_exe': {
+        'includes': 'atexit',
+        'zip_include_packages': '*',
+        'zip_exclude_packages': None,
+    }
+}
+
+executables = [
+    Executable('VoiceRecorder.pyw', base=base)
+]
+
+setup(
+    name='VoiceRecorder',
+    version=__version__,
+    description='VoiceRecorder is a simple application for voice record',
+    options=options,
+    executables=executables
+)
