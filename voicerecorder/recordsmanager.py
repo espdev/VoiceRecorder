8b3b781f2a9aba47dff6982a2f6615703e7dd9cd
Evgeny Prilepin <esp.home@gmail.com>
2025-11-03 11:47:53 +0000

    Refactoring

--- voicerecorder/recordsmanager.py
+++ voicerecorder/recordsmanager.py
@@ -5,11 +5,47 @@ from PySide6.QtCore import QEvent, QModelIndex, QObject, Qt, QUrl
 from PySide6.QtGui import QDesktopServices, QKeyEvent
 from PySide6.QtMultimedia import QMediaFormat
 from PySide6.QtSql import QSqlDatabase, QSqlQuery, QSqlTableModel
-from PySide6.QtWidgets import QHeaderView, QTableView
+from PySide6.QtWidgets import QHeaderView, QStyledItemDelegate, QTableView
 
-from .recordsmodel import DateDelegate, DurationDelegate, RecordsTableModel
 from .settings import Settings
-from .utils import format_audio_format
+from .utils import format_audio_format, format_duration, format_timestamp
+
+
+class DateDelegate(QStyledItemDelegate):
+    def __init__(self, settings: Settings, parent: QObject | None = None):
+        super().__init__(parent)
+        self._settings = settings
+
+    def displayText(self, value, locale):
+        created = int(value)
+        return format_timestamp(created, self._settings.record_table_format())
+
+    def createEditor(self, parent, option, index):
+        return None
+
+
+class DurationDelegate(QStyledItemDelegate):
+    def displayText(self, value, locale):
+        duration = int(value)
+        return format_duration(duration)
+
+    def initStyleOption(self, option, index):
+        super().initStyleOption(option, index)
+        option.displayAlignment = Qt.AlignmentFlag.AlignCenter
+
+    def createEditor(self, parent, option, index):
+        return None
+
+
+class RecordsTableModel(QSqlTableModel):
+    def data(self, index, role=Qt.ItemDataRole.DisplayRole):
+        if not index.isValid():
+            return None
+
+        if role == Qt.ItemDataRole.ToolTipRole and index.column() == self.fieldIndex('description'):
+            return super().data(index, Qt.ItemDataRole.DisplayRole)
+
+        return super().data(index, role)
 
 
 @dataclass
